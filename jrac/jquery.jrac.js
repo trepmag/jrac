336d42ba06d901ae5346cee0089b080a90716c34
trepmag <cgampert@gmail.com>
2011-06-29 10:46:00 +0200

    Various code rewrite.

--- jrac/jquery.jrac.js
+++ jrac/jquery.jrac.js
@@ -35,11 +35,11 @@
       }
 
       // Prepare image
-      var $image = $(this).hide().css('position','absolute').wrap('<span class="jrac_container"><div class="jrac_viewport"></div></span>');
+      var $image = $(this).hide().css('position','absolute').wrap('<div class="jrac_container"><div class="jrac_viewport"></div></div>');
 
       // Get viewport and container references
       var $viewport = $image.parent();
-      var $container = $image.parents('.jrac_container');
+      var $container = $viewport.parent();
 
       // Add a waiting on load input image
       var $loading = $('<div class="jrac_loading" />');
@@ -81,16 +81,16 @@
         $image.draggable({
           drag: function(event, ui) {
             if (ui.position.left != ui.originalPosition.left) {
-              $viewport.observator.notify_crop_x();
+              $viewport.observator.notify('crop_x', $viewport.observator.crop_position_x());
             }
             if (ui.position.left != ui.originalPosition.left) {
-              $viewport.observator.notify_crop_y();
+              $viewport.observator.notify('crop_y', $viewport.observator.crop_position_y());
             }
           }
         });
 
-        // Build the select crop
-        var $crop = $('<div class="jrac_select" />')
+        // Build the crop element
+        var $crop = $('<div class="jrac_crop" />')
         .css({
           'width': settings.crop_width,
           'height': settings.crop_height,
@@ -100,10 +100,10 @@
           containment: $viewport,
           drag: function(event, ui) {
             if (ui.position.left != ui.originalPosition.left) {
-              $viewport.observator.notify_crop_x();
+              $viewport.observator.notify('crop_x', $viewport.observator.crop_position_x());
             }
             if (ui.position.left != ui.originalPosition.left) {
-              $viewport.observator.notify_crop_y();
+              $viewport.observator.notify('crop_y', $viewport.observator.crop_position_y());
             }
           }
         });
@@ -125,6 +125,7 @@
         // Extend viewport witch usefull objects as it will be exposed to user
         // functions interface
         $.extend($viewport, {
+          $container: $container,
           $image: $image,
           $crop: $crop,
           // Let me introduce the following Terminator's friend which handle the
@@ -151,52 +152,41 @@
                 var onevent_callback = this.items[event_name].callback;
                 element.trigger(event_name,[$viewport, value]);
                 if ($.isFunction(onevent_callback)) {
-                  onevent_callback.call($viewport, event_name, this.items[event_name].element, value);
+                  onevent_callback.call($viewport, event_name, element, value);
                 }
               }
               $image.trigger('viewport_events',[$viewport]);
             },
             notify_all: function() {
-              this.notify_crop_x();
-              this.notify_crop_y();
-              this.notify_crop_size();
-              this.notify_image_size();
-            },
-            notify_crop_x: function() { // relative to image
-              var position = this.crop_position();
-              this.notify('crop_x', position.x);
-            },
-            notify_crop_y: function() { // relative to image
-              var position = this.crop_position();
-              this.notify('crop_y', position.y);
-            },
-            notify_crop_size: function() {
+              this.notify('crop_x', this.crop_position_x());
+              this.notify('crop_y', this.crop_position_y());
               this.notify('crop_width', $crop.width());
               this.notify('crop_height', $crop.height());
-            },
-            notify_image_size: function() {
               this.notify('image_width', $image.width());
               this.notify('image_height', $image.height());
             },
-            // Return crop position relative to $image
-            crop_position: function() {
-              return {
-                x: $crop.position().left - $image.position().left,
-                y: $crop.position().top - $image.position().top
-              };
+            // Return crop x position relative to $image
+            crop_position_x: function() {
+              return $crop.position().left - $image.position().left;
+            },
+            // Return crop y position relative to $image
+            crop_position_y: function() {
+              return $crop.position().top - $image.position().top;
             },
             // Does the crop is completely inside the image?
             crop_consistent: function() {
-              return this.crop_position().x>=0 && this.crop_position().y>=0
-              && this.crop_position().x + $crop.width()<=$image.width()
-              && this.crop_position().y + $crop.height()<=$image.height();
+              return this.crop_position_x()>=0 && this.crop_position_y()>=0
+              && this.crop_position_x() + $crop.width()<=$image.width()
+              && this.crop_position_y() + $crop.height()<=$image.height();
             },
             // Set a property (which his name is one of the event) with a given
             // value then notify this operation
-            set_property: function(what,value) {
+            set_property: function(that, value) {
               value = parseInt(value);
-              if (isNaN(value)) return;
-              switch (what) {
+              if (isNaN(value)) {
+                return;
+              }
+              switch (that) {
                 case 'crop_x':
                   $crop.css('left',value + $image.position().left);
                   break;
@@ -216,7 +206,7 @@
                   $image.height(value);
                   break;
               }
-              this.notify(what,value);
+              this.notify(that, value);
             }
           }
         })
